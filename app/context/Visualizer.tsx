8aa56875508a179b39b1155b543ec7a8bde70596
Devon Dunham <itisdevondunham@gmail.com>
2024-07-31 15:12:37 -0400

    init + beginning ui

---
+++ app/context/Visualizer.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+import { SortingAlgorithmType } from "../lib/types";
+import { MAX_ANIMATION_SPEED } from "../lib/utils";
+
+interface SortingAlgorithmContextType {
+  arrayToSort: number[];
+  selectedAlgorithm: SortingAlgorithmType;
+  isSorting: boolean;
+  setSelectedAlgorithm: (algorithm: SortingAlgorithmType) => void;
+  setIsSorting: (isSorting: boolean) => void;
+  animationSpeed: number;
+  setAnimationSpeed: (speed: number) => void;
+  isAnimationComplete: boolean;
+  setIsAnimationComplete: (isComplete: boolean) => void;
+  resetArrayAndAnimation: () => void;
+  runAnimation: () => void;
+}
+
+const SortingAlgorithmContext = createContext<
+  SortingAlgorithmContextType | undefined
+>(undefined);
+
+export const SortingAlgorithmProvidor = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [arrayToSort, setArrayToSort] = useState<number[]>([]);
+  const [selectedAlgorithm, setSelectedAlgorithm] =
+    useState<SortingAlgorithmType>("bubble");
+
+  const [isSorting, setIsSorting] = useState<boolean>(false);
+  const [animationSpeed, setAnimationSpeed] =
+    useState<number>(MAX_ANIMATION_SPEED);
+  const [isAnimationComplete, setIsAnimationComplete] =
+    useState<boolean>(false);
+
+  const resetArrayAndAnimation = () => {};
+
+  const runAnimation = () => {};
+
+  const value = {
+    arrayToSort,
+    setArrayToSort,
+    selectedAlgorithm,
+    setSelectedAlgorithm,
+    isSorting,
+    setIsSorting,
+    animationSpeed,
+    setAnimationSpeed,
+    isAnimationComplete,
+    setIsAnimationComplete,
+    resetArrayAndAnimation,
+    runAnimation,
+  };
+
+  return (
+    <SortingAlgorithmContext.Provider value={value}>
+      {children}
+    </SortingAlgorithmContext.Provider>
+  );
+};
+
+export const useSortingAlgorithmContext = () => {
+  const context = useContext(SortingAlgorithmContext);
+
+  if (!context) {
+    throw new Error(
+      "func must be used within a SortingAlgorithmContextProvidor"
+    );
+  }
+
+  return context;
+};
